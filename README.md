aecb0ccc81ececbf9a0be6bea9e18af7c4026739
quiquek007 <quiquek007@gmail.com>
2018-09-17 17:30:33 +0300

    Solution

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 ## Task
 
-Implement function `check(str, bracketsConfig)`, that for given brackets sequence will return `true` if it is correct and `false` otherwise
+Implement function `check(str, bracketsConfig)`,
+that for given brackets sequence will return `true` if it is correct and `false` otherwise.
 
-In the second param there is `bracketsConfig` - the array of pairs open-closed brackets. Each subarray includes only 2 elements - opening and closing bracket
+In the second param there is `bracketsConfig` - the array of pairs open-closed brackets.
+Each subarray includes only 2 elements - opening and closing bracket
 
 ```js
 check('()', [['(', ')']]) // -> true
--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 == 1) return false;
+  // let log = console.log;
+  let strLength = str.length;
+  let scope = [];
+  let level = 0;
+  for (let i = 0; i < strLength; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (level < 0) return false;
+      if (str[i] === bracketsConfig[j][0] && (bracketsConfig[j][0] !== bracketsConfig[j][1])) {
+        if (scope[level] == undefined) scope[level] = str[i];
+        else {
+          // if (bracketsConfig[j][0] === bracketsConfig[j][1]) level++;
+          // if (scope[level][scope[level].length-1] === str[i]) log("bu");
+          scope[level] += str[i];
+        }
+        level++;
+      } else if (str[i] === bracketsConfig[j][1] && (bracketsConfig[j][0] !== bracketsConfig[j][1])) {
+        level--;
+        scope[level] += str[i];
+      } else if (str[i] === bracketsConfig[j][0] && (bracketsConfig[j][0] === bracketsConfig[j][1])) {
+        if (scope[level] == undefined) {
+          if (level > 0) {
+            if (scope[level - 1][scope[level - 1].length - 1] === str[i]) {
+              --level;
+              scope[level] += str[i];
+            } else {
+              scope[level] = str[i];
+              ++level;
+            }
+          } else {
+            scope[level] = str[i];
+            ++level;
+          }
+        } else {
+          if (level>0) {
+            if (scope[level - 1][scope[level - 1].length - 1] === str[i]) {
+              --level;
+              scope[level] += str[i];
+            } else {
+              scope[level] += str[i];
+              ++level;
+            }
+          } else {
+            scope[level] += str[i];
+            ++level;
+          }
+        }
+      }
+    }
+  }
+  for(let i=0;i<scope.length;i++){
+    if (scope[i].length %2 == 1) return false;
+  }
+  // log("scope: " + scope);
+  for(let i=0;i<scope.length;i++){
+    for(let j=0;j<scope[i].length;j+=2)
+      for(let k=0;k<bracketsConfig.length;k++){
+        if (scope[i][j] == bracketsConfig[k][0]){
+          if (scope[i][j+1] !== bracketsConfig[k][1]) return false;
+        }
+      }
+  }
+  return true;
+};
